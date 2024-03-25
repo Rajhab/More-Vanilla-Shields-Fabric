a8567d04830886f1d583b6237d8b41ef1a0d15e2
Rajhab <rajhabcurseforge@gmail.com>
2024-03-25 13:08:12 +0100

    Initial Commit 0.0.0-1.20.4-alpha

---
+++ src/main/java/com/rajhab/morevanillashields_mod/MoreVanillaShieldsClient.java
@@ -0,0 +1,11 @@
+package com.rajhab.morevanillashields_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreVanillaShieldsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
