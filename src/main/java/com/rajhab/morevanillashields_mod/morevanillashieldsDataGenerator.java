36701c4aa1b59fa586e58145c93fbefad00eb437
Rajhab <rajhabcurseforge@gmail.com>
2024-05-07 09:24:42 +0200

    Changelog 1.0.0-1.20.6-fabric:
    - Ported from 1.0.0-1.20.4-fabric to 1.0.0-1.20.6-fabric

--- src/main/java/com/rajhab/morevanillashields_mod/morevanillashieldsDataGenerator.java
+++ src/main/java/com/rajhab/morevanillashields_mod/morevanillashieldsDataGenerator.java
@@ -3,7 +3,7 @@ package com.rajhab.morevanillashields_mod;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class MoreVanillaShieldsDataGenerator implements DataGeneratorEntrypoint {
+public class morevanillashieldsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
