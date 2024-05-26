33d9f3bc5cf6a56413f7a87a31671c6ff7bd5663
Rajhab <rajhabcurseforge@gmail.com>
2024-05-26 18:36:49 +0200

    Changelog 1.0.1-1.18.2:
    - Updated Fabric to 0.15.11
    - Added Banner Support for every non-translucent Shield
    - Enhanced Creative Mode Tab
    - Fixed Shield Scaling
    - Added Shield Decoration Recipes to Misc Tab (recipe book)

--- src/main/java/com/rajhab/morevanillashields_mod/morevanillashields.java
+++ src/main/java/com/rajhab/morevanillashields_mod/morevanillashields.java
@@ -2,18 +2,37 @@ package com.rajhab.morevanillashields_mod;
 
 import com.rajhab.morevanillashields_mod.config.ModConfigs;
 import com.rajhab.morevanillashields_mod.item.ModItems;
+import com.rajhab.morevanillashields_mod.util.ModModelPredicateProvider;
 import com.rajhab.morevanillashields_mod.util.ModRegistries;
+import com.rajhab.morevanillashields_mod.util.ModShieldDecoratorRecipe;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.minecraft.recipe.RecipeType;
+import net.minecraft.recipe.SpecialRecipeSerializer;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 //Special Thanks to KaupenJoe for perfect Fabric Tutorials <3. I miss Forge :(
+//Thanks to StellarWind22 for code of his Fabric-Shield-Lib
 
 public class morevanillashields implements ModInitializer {
 
 	public static final String MOD_ID = "morevanillashields";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	public static final SpecialRecipeSerializer<ModShieldDecoratorRecipe> MOD_SHIELD_DECORATION_SERIALIZER;
+	public static final RecipeType<ModShieldDecoratorRecipe> MOD_SHIELD_DECORATION;
+
+	static {
+		//Registering Banner Recipe (Lib only)
+		MOD_SHIELD_DECORATION = Registry.register(Registry.RECIPE_TYPE, new Identifier(MOD_ID, "mod_shield_decoration"), new RecipeType<ModShieldDecoratorRecipe>() {
+			@Override
+			public String toString() {return "test_recipe";}
+		});
+		MOD_SHIELD_DECORATION_SERIALIZER = Registry.register(Registry.RECIPE_SERIALIZER, new Identifier(MOD_ID, "mod_shield_decoration"), new SpecialRecipeSerializer<>(ModShieldDecoratorRecipe::new));
+	}
 
 	@Override
 	public void onInitialize() {
@@ -23,6 +42,7 @@ public class morevanillashields implements ModInitializer {
 		ModConfigs.registerConfigs();
 		ModItems.registerModItems();
 		ModRegistries.registerModStuffs();
+		ModModelPredicateProvider.registerModModels();
 
 		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
 			LOGGER.info(MOD_ID + ": This mod is now running on a server. Please report any error or bugs.");
---
+++ src/main/java/com/rajhab/morevanillashields_mod/util/ModShieldDecoratorRecipe.java
@@ -0,0 +1,108 @@
+package com.rajhab.morevanillashields_mod.util;
+
+import com.rajhab.morevanillashields_mod.item.ModItems;
+import com.rajhab.morevanillashields_mod.morevanillashields;
+import net.minecraft.inventory.CraftingInventory;
+import net.minecraft.item.BannerItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.recipe.RecipeSerializer;
+import net.minecraft.recipe.ShieldDecorationRecipe;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.World;
+
+public class ModShieldDecoratorRecipe extends ShieldDecorationRecipe {
+
+    public ModShieldDecoratorRecipe(Identifier identifier) {
+        super(identifier);
+    }
+
+    @Override
+    public boolean matches(CraftingInventory craftingInventory, World world) {
+        ItemStack itemStack = ItemStack.EMPTY;
+        ItemStack itemStack2 = ItemStack.EMPTY;
+
+        for (int i = 0; i < craftingInventory.size(); ++i) {
+            ItemStack itemStack3 = craftingInventory.getStack(i);
+            if (!itemStack3.isEmpty()) {
+                if (itemStack3.getItem() instanceof BannerItem) {
+                    if (!itemStack2.isEmpty()) {
+                        return false;
+                    }
+
+                    itemStack2 = itemStack3;
+                } else {
+
+                    if (!(itemStack3.isOf(ModItems.LEATHER_SHIELD)) &&
+                            !(itemStack3.isOf(ModItems.GOLD_SHIELD)) &&
+                            !(itemStack3.isOf(ModItems.DIAMOND_SHIELD)) &&
+                            !(itemStack3.isOf(ModItems.NETHERITE_SHIELD)) &&
+                            !(itemStack3.isOf(ModItems.EMERALD_SHIELD)) &&
+                            !(itemStack3.isOf(ModItems.AMETHYST_SHIELD)) &&
+                            !(itemStack3.isOf(ModItems.OBSIDIAN_SHIELD)) &&
+                            !(itemStack3.isOf(ModItems.COAL_SHIELD)) &&
+                            !(itemStack3.isOf(ModItems.END_CRYSTAL_SHIELD))
+                    ) {
+                        return false;
+                    }
+
+                    if (!itemStack.isEmpty()) {
+                        return false;
+                    }
+
+                    if (itemStack3.getSubNbt("BlockEntityTag") != null) {
+                        return false;
+                    }
+
+                    itemStack = itemStack3;
+                }
+            }
+        }
+
+        return !itemStack.isEmpty() && !itemStack2.isEmpty();
+    }
+
+
+    @Override
+    public ItemStack craft(CraftingInventory craftingInventory) {
+        ItemStack itemStack = ItemStack.EMPTY;
+        ItemStack itemStack2 = ItemStack.EMPTY;
+
+        for (int i = 0; i < craftingInventory.size(); ++i) {
+            ItemStack itemStack3 = craftingInventory.getStack(i);
+            if (!itemStack3.isEmpty()) {
+                if (itemStack3.getItem() instanceof BannerItem) {
+                    itemStack = itemStack3;
+                } else if ((itemStack3.isOf(ModItems.LEATHER_SHIELD)) ||
+                        (itemStack3.isOf(ModItems.GOLD_SHIELD)) ||
+                        (itemStack3.isOf(ModItems.DIAMOND_SHIELD)) ||
+                        (itemStack3.isOf(ModItems.NETHERITE_SHIELD)) ||
+                        (itemStack3.isOf(ModItems.EMERALD_SHIELD)) ||
+                        (itemStack3.isOf(ModItems.AMETHYST_SHIELD)) ||
+                        (itemStack3.isOf(ModItems.OBSIDIAN_SHIELD)) ||
+                        (itemStack3.isOf(ModItems.COAL_SHIELD)) ||
+                        (itemStack3.isOf(ModItems.END_CRYSTAL_SHIELD))
+                ) {
+                    itemStack2 = itemStack3.copy();
+                }
+            }
+        }
+
+        if (!itemStack2.isEmpty()) {
+            NbtCompound nbtCompound = itemStack.getSubNbt("BlockEntityTag");
+            NbtCompound nbtCompound2 = nbtCompound == null ? new NbtCompound() : nbtCompound.copy();
+            nbtCompound2.putInt("Base", ((BannerItem) itemStack.getItem()).getColor().getId());
+            itemStack2.setSubNbt("BlockEntityTag", nbtCompound2);
+        }
+        return itemStack2;
+    }
+
+    public boolean fits(int width, int height) {
+        return width * height >= 2;
+    }
+
+    @Override
+    public RecipeSerializer<?> getSerializer() {
+        return morevanillashields.MOD_SHIELD_DECORATION_SERIALIZER;
+    }
+}
