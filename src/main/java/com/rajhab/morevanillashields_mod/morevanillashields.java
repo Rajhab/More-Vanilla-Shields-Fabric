ae3d473142e451a74fca5d628ce77243f395df46
Rajhab <rajhabcurseforge@gmail.com>
2024-10-23 23:13:25 +0200

    Changelog 1.0.3-1.21:
    - Updated to 0.16.7
    - Fixed typo in german translation
    - Fixed Redstone Shield Particle Density
    - Made the End Crystal Shield explosive (turned on by default. To disable go into config)
    - Made End Crystal Shield Explosion serversided

--- src/main/java/com/rajhab/morevanillashields_mod/morevanillashields.java
+++ src/main/java/com/rajhab/morevanillashields_mod/morevanillashields.java
@@ -1,5 +1,6 @@
 package com.rajhab.morevanillashields_mod;
 
+import com.rajhab.morevanillashields_mod.event.ShieldEventHandler;
 import com.rajhab.morevanillashields_mod.item.ModItems;
 import com.rajhab.morevanillashields_mod.util.ModShieldDecoratorRecipe;
 import com.rajhab.morevanillashields_mod.util.ModRegistries;
@@ -37,6 +38,7 @@ public class morevanillashields implements ModInitializer {
 
 		ModItems.registerModItems();
 		ModRegistries.registerModStuffs();
+		ShieldEventHandler.registerEventStuff();
 
 		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
 			LOGGER.info(MOD_ID + ": This mod is now running on a server. Please report any error or bugs.");
