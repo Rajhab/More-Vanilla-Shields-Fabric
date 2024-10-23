1c6184d98ca1ada7c92f1adbd1ceefacce064eb9
Rajhab <rajhabcurseforge@gmail.com>
2024-10-24 00:42:07 +0200

    Changelog 1.0.5-1.20.1:
    - Updated to 0.16.7
    - Fixed typo in german translation
    - Fixed Redstone Shield Particle Density
    - Made the End Crystal Shield explosive (turned on by default. To disable go into config)
    - Made End Crystal Shield Explosion serversided

--- src/main/java/com/rajhab/morevanillashields_mod/event/ShieldEventHandler.java
+++ src/main/java/com/rajhab/morevanillashields_mod/event/ShieldEventHandler.java
@@ -3,6 +3,7 @@ package com.rajhab.morevanillashields_mod.event;
 import com.rajhab.morevanillashields_mod.item.ModItems;
 import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.Vec3d;
@@ -39,6 +40,26 @@ public class ShieldEventHandler {
     private static void createExplosion(ServerWorld serverWorld, Vec3d pos, LivingEntity serverLivingEntity) {
         if (!serverWorld.isClient && serverWorld instanceof ServerWorld) {
             serverWorld.createExplosion(serverLivingEntity, pos.getX(), pos.getY(), pos.getZ(), 5.0F, World.ExplosionSourceType.NONE);
+
+            applyShieldDamage(serverLivingEntity);
         }
+    }
+
+    private static void applyShieldDamage(LivingEntity livingEntity) {
+        ItemStack mainHandStack = livingEntity.getStackInHand(Hand.MAIN_HAND);
+        ItemStack offHandStack = livingEntity.getStackInHand(Hand.OFF_HAND);
+
+        if (mainHandStack.getItem() == ModItems.END_CRYSTAL_SHIELD) {
+            damageShield(mainHandStack, livingEntity);
+        }
+        if (offHandStack.getItem() == ModItems.END_CRYSTAL_SHIELD) {
+            damageShield(offHandStack, livingEntity);
+        }
+    }
+
+    private static void damageShield(ItemStack shield, LivingEntity user) {
+        int fixedDamageAmount = 175;
+        shield.damage(fixedDamageAmount, user, (entity) -> {
+        });
     }
 }
